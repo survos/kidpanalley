1a6441490a6776544bf9da2ec85481719b88c391
Tac Tacelosky <tacman@gmail.com>
2022-12-05 16:59:10 -0500

    remove sensio framework

--- src/Request/ParamConverter/VideoParamConverter.php
+++ src/Request/ParamConverter/VideoParamConverter.php
@@ -4,74 +4,37 @@ namespace App\Request\ParamConverter;
 
 use App\Entity\Video;
 
-use Doctrine\Persistence\ManagerRegistry;
-use Exception;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class VideoParamConverter implements ParamConverterInterface
+class VideoParamConverter implements ValueResolverInterface
 {
+    public function resolve(Request $request, ArgumentMetadata $argument): array
+    {
+        // get the argument type (e.g. BookingId)
+        $argumentType = $argument->getType();
+        switch ($argumentType) {
+            case Video::class:
+                $repository = $this->entityManager->getRepository($argumentType);
+                $value = $request->attributes->get('videoId');
+                if (!is_string($value)) {
+                    return [];
+                }
+                // Try to find video by its uniqueParameters.  Inspect the class to get this
+                return [$repository->findOneBy(['youtubeId' => $value])];
 
-    /**
-     * @param ManagerRegistry $registry Manager registry
-     */
-    public function __construct(private readonly ?\Doctrine\Persistence\ManagerRegistry $registry = null)
+        }
+
+        return [];
+    }
+
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    )
     {
     }
 
-    /**
-     * {@inheritdoc}
-     *
-     * Check, if object supported by our converter
-     */
-    public function supports(ParamConverter $configuration): bool
-    {
-        return Video::class == $configuration->getClass();
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * Applies converting
-     *
-     * @throws \InvalidArgumentException When route attributes are missing
-     * @throws NotFoundHttpException     When object not found
-     * @throws Exception
-     */
-    public function apply(Request $request, ParamConverter $configuration)
-    {
-        $params = $request->attributes->get('_route_params');
-
-//        if (isset($params['videoId']) && ($videoId = $request->attributes->get('videoId')))
-
-        $videoId = $request->attributes->get('videoId');
-        if ($videoId === 'undefined') {
-            throw new Exception("Invalid videoId " . $videoId);
-        }
-
-        // Check, if route attributes exists
-        if (null === $videoId ) {
-            if (!isset($params['videoId'])) {
-                return; // no videoId in the route, so leave.  Could throw an exception.
-            }
-        }
-
-        // Get actual entity manager for class.  We can also pass it in, but that won't work for the doctrine tree extension.
-        $em = $this->registry->getManagerForClass($configuration->getClass());
-        $repository = $em->getRepository($configuration->getClass());
-
-        // Try to find video by its uniqueParameters.  Inspect the class to get this
-        $video = $repository->findOneBy(['youtubeId' => $videoId]);
-
-        if (null === $video || !($video instanceof Video)) {
-            throw new NotFoundHttpException(sprintf('%s %s object not found.', $videoId, $configuration->getClass()));
-        }
-
-        // Map found video to the route's parameter
-        $request->attributes->set($configuration->getName(), $video);
-        return true;
-    }
 
 }
