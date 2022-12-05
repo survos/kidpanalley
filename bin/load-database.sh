79b28b6134cf2aea677d79b874bafa2cc7194850
Tac Tacelosky <tacman@gmail.com>
2022-12-05 16:35:31 -0500

    bump to Symfony 6.2

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -1,13 +1,5 @@
 dbname=kpa
-
-#echo "create user main WITH ENCRYPTED PASSWORD 'main';" | sudo -u postgres psql
-echo "drop database if exists $dbname; create database $dbname; grant all privileges on database $dbname to main; " | sudo -u postgres psql
-
-echo "CREATE EXTENSION pg_trgm;" | sudo -u postgres psql -d $dbname
-echo "CREATE EXTENSION hstore;" | sudo -u postgres psql -d $dbname
-echo "database $dbname now ready for migrations or restore"
-
-#bin/console doctrine:schema:update --force
+bin/console doctrine:database:drop --force && bin/console doctrine:database:create
 bin/console doctrine:migrations:migrate -n
 bin/create-admins.sh
 bin/console app:load-data
