89b582c307e789b98a09a25bcf0641f98f4ea639
Tac Tacelosky <tacman@gmail.com>
2024-02-04 09:16:27 -0500

    update recipe

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,6 @@
 import 'bootstrap';
 
 import './styles/app.css';
-// start the Stimulus application
-console.log('stimulus...');
 import './bootstrap.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
