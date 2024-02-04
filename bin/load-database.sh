08b6d0efd2e0cde41e9059fd0fa7bcfd7c765b0c
Tac Tacelosky <tacman@gmail.com>
2024-02-04 09:38:36 -0500

    dont recreate database

--- bin/load-database.sh
+++ bin/load-database.sh
@@ -1,7 +1,6 @@
 dbname=kpa
-symfony console doctrine:database:drop --force && symfony console doctrine:database:create
+#symfony console doctrine:database:drop --force && symfony console doctrine:database:create
 symfony console doctrine:migrations:migrate -n
-symfony console d:schema:update --force --complete
 bin/create-admins.sh
 bin/console app:load-data -v
 bin/console grid:index --reset
