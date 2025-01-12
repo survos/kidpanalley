7d5c8dcb1baeffe48858923812a7692c8ba56a77
Tac Tacelosky <tacman@gmail.com>
2025-01-12 07:53:08 -0500

    prep for more meili

--- src/Controller/SongCollectionController.php
+++ src/Controller/SongCollectionController.php
@@ -6,6 +6,7 @@ use App\Entity\Song;
 use App\Entity\Video;
 use App\Form\SongType;
 use App\Repository\SongRepository;
+use Survos\ApiGrid\Service\MeiliService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,13 +15,16 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route(path: '/songs')]
 class SongCollectionController extends AbstractController
 {
-    public function __construct(private readonly \Doctrine\Persistence\ManagerRegistry $managerRegistry)
+    public function __construct(
+        private MeiliService $meiliService,
+        private readonly \Doctrine\Persistence\ManagerRegistry $managerRegistry)
     {
 
     }
     #[Route(path: '/browse/{apiRoute}', name: 'song_index', methods: ['GET'])]
     public function index(string $apiRoute=Song::MEILI_ROUTE) : Response
     {
+//        $this->meiliService->getConfig()
         return $this->render('song/index.html.twig', [
                 'apiRoute' => $apiRoute,
                 'class' => Song::class,
